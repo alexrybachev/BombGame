d351fd94b9d93df2eb62397d2d0008e3529de131
s9888556 <s9888556@yandex.ru>
2023-08-07 16:42:52 +0500

    add SM8Bomb app

---
+++ SM8Bomb/ColorsExtension.swift
@@ -0,0 +1,13 @@
+//
+//  ColorsExtension.swift
+//  SM8Bomb
+//
+//  Created by Сергей П on 07.08.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static let yellowText = UIColor(named: "yellowText")
+    static let violetText = UIColor(named: "violetText")
+}
