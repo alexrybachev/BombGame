f212b9bbc139faec2b04b2f706aac428c05ad664
s9888556 <s9888556@yandex.ru>
2023-08-09 18:40:49 +0500

    add MainViewController

---
+++ SM8Bomb/Controllers/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MainViewController.swift
+//  SM8Bomb
+//
+//  Created by Сергей П on 09.08.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func categoryButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func rulesButtonPressed(_ sender: UIButton) {
+    }
+    
+}
