fe6422ed4fb7c0641c6bf51db0be8e8cf21c064b
s9888556 <s9888556@yandex.ru>
2023-08-12 13:28:04 +0500

    add NavBarLabel

--- SM8Bomb/Controllers/RulesViewControllerTwo.swift
+++ SM8Bomb/Controllers/RulesViewControllerTwo.swift
@@ -12,7 +12,7 @@ class RulesViewControllerTwo: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupNavBar("Помощь")
     }
 
 
